c940fc7cdd03467a67526404a11217bead0710f4
ManilaSR57 <tsebekov2003@snu.ac.kr>
2024-03-13 08:16:21 +0900

    Wrote Code for Single Game
    Problem of Tie still has to be solved

---
+++ javascript.js
@@ -0,0 +1,61 @@
+function getComputerChoice() {
+    //Get random integer among 0, 1 or 2 that will be used as rock, paper or scissors
+    let computerChoice = Math.floor(Math.random() * 3)
+    switch (computerChoice) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
+
+function getPlayerChoice() {
+    playerSelection = prompt("Rock! Paper! Scissors!").toLowerCase();
+
+    if (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+        console.log('Error! Enter Rock, Paper or Scissor!')
+    }
+    else {
+        return playerSelection;
+    }
+}
+
+
+function playRound() {
+    let playerSelection = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'rock') {
+            return 'Tie! Rock vs Rock';
+        } else if (computerSelection === 'paper') {
+            return 'You Lose! Paper beats Rock!';
+        } else if (computerSelection === 'scissors') {
+            return 'You Win! Rock beats Scissisors!';
+        }
+    }
+    
+    if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            return 'You Win! Paper beats Rock!';
+        } else if (computerSelection === 'paper') {
+            return 'Tie! Paper vs Paper';
+        } else if (computerSelection === 'scissors') {
+            return 'You Lose! Scissors beat Paper!';
+        }
+    }
+
+    if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            return 'You Lose! Rock beats Scissors!';
+        } else if (computerSelection === 'paper') {
+            return 'You Win! Scissors beat Paper!';
+        } else if (computerSelection === 'scissors') {
+            return 'Tie! Scissors vs Scissors';
+        }
+    }
+}
+
+console.log(playRound());
